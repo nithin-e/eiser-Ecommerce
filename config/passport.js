78f8b8197a6e93260b5200fdfc9d7266fbc29ef6
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2024-08-19 20:07:11 +0530

    passport config updated

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID : process.env.CLIENT_ID,
             clientSecret : process.env.CLIENT_SECRET,
-            callbackURL : "http://localhost:3000/auth/google/callback",
+            callbackURL : "https://eiser.online/auth/google/callback",
             scope : ["profile","email"]
         },
         function (accessToken , refreshToken , profile , callback) {
