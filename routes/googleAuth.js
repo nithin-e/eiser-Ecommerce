2a4145761f1e368839f25aeb0803bca3b8eb5c62
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2024-08-20 10:10:17 +0530

    gp

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -41,8 +41,8 @@ router.get('/auth/google', (req, res, next) => {
     console.log('Attempting Google authentication');
     passport.authenticate('google', { scope: ['profile', 'email'] })(req, res, next);
   });
-  
-router.get("/auth/google/callback", passport.authenticate('google', {
+
+router.get("/google/callback", passport.authenticate('google', {
   failureRedirect: "/login/failed"
 }), googleController.googleLoginSucces);
 
